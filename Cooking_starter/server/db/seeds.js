7031ee612ad507561b3e5c139a4c8f21aba33287
Akwasi Atakora-Acheampong <aa.acheampong1@gmail.com>
2021-08-07 15:33:41 +0100

    seed data added

---
+++ Cooking_starter/server/db/seeds.js
@@ -0,0 +1,319 @@
+use recipes;
+
+db.dropDatabase();
+
+db.recipes.insertMany([
+
+{
+    "id": 539480,
+    "imageType": "jpg",
+    "title": "Strawberry Shortcake Pancakes",
+    "readyInMinutes": 45,
+    "servings": 4,
+    "sourceUrl": "http://www.crumbblog.com/2011/05/stacked-up-strawberry-shortcake.html"
+    },
+    {
+    "id": 512530,
+    "imageType": "jpg",
+    "title": "Strawberry Shortcake Pancakes (+ GIVEAWAY!)",
+    "readyInMinutes": 30,
+    "servings": 10,
+    "sourceUrl": "http://www.theroastedroot.net/gluten-free-strawberry-shortcake-pancakes/"
+    },
+    {
+    "id": 551855,
+    "imageType": "jpg",
+    "title": "Gluten-free strawberry shortcake pancakes",
+    "readyInMinutes": 45,
+    "servings": 8,
+    "sourceUrl": "http://www.adashofcompassion.com/2013/05/gluten-free-strawberry-shortcake-pancakes/"
+    },
+    {
+    "id": 994593,
+    "imageType": "jpg",
+    "title": "Skinny Strawberry Shortcake Gluten Free Pancakes",
+    "readyInMinutes": 7,
+    "servings": 6,
+    "sourceUrl": "https://ohsweetbasil.com/skinny-strawberry-shortcake-gluten-free-pancakes-recipe/"
+    },
+    {
+    "id": 469936,
+    "imageType": "jpg",
+    "title": "Easy Vegan and Gluten-Free Pancakes (Strawberry Shortcake + Whipped Cream)",
+    "readyInMinutes": 30,
+    "servings": 9,
+    "sourceUrl": "http://ohsheglows.com/2014/02/25/easy-vegan-and-gluten-free-pancakes-strawberry-shortcake-w-whipped-cream/"
+    },
+    {
+    "id": 1100250,
+    "imageType": "jpg",
+    "title": "MISS SHORTCAKE DONUTS (STRAWBERRY SHORTCAKE)",
+    "readyInMinutes": 18,
+    "servings": 8,
+    "sourceUrl": "http://littlespicejar.com/miss-shortcake-donuts/"
+    },
+    {
+    "id": 716417,
+    "imageType": "jpg",
+    "title": "Strawberry Shortcake w. Mini Strawberry PopTarts",
+    "readyInMinutes": 45,
+    "servings": 4,
+    "sourceUrl": "https://spoonacular.com/strawberry-shortcake-w-mini-strawberry-poptarts-716417"
+    },
+    {
+    "id": 605456,
+    "imageType": "jpg",
+    "title": "Strawberry Shortcake",
+    "readyInMinutes": 75,
+    "servings": 12,
+    "sourceUrl": "http://www.browneyedbaker.com/2014/07/15/strawberry-shortcake-recipe/"
+    },
+    {
+    "id": 58012,
+    "imageType": "jpg",
+    "title": "Strawberry Shortcake",
+    "readyInMinutes": 60,
+    "servings": 8,
+    "sourceUrl": "http://www.marthastewart.com/336107/strawberry-shortcake"
+    },
+    {
+        "id": 149340,
+        "imageType": "jpg",
+        "title": "Prosciutto-Wrapped Shrimp with Mozzarella and Basil",
+        "readyInMinutes": 60,
+        "servings": 4,
+        "sourceUrl": "http://www.seriouseats.com/recipes/2013/08/prosciutto-wrapped-shrimp-with-mozzarella-and-basil.html"
+        },
+        {
+        "id": 551304,
+        "imageType": "jpg",
+        "title": "pass the prosciutto – prosciutto wrapped pear with parmesan",
+        "readyInMinutes": 18,
+        "servings": 12,
+        "sourceUrl": "http://www.greensnchocolate.com/2013/11/pass-the-prosciutto-prosciutto-wrapped-pear-with-parmesan.html"
+        },
+        {
+        "id": 592959,
+        "imageType": "jpg",
+        "title": "Caramelized Onion, Prosciutto and Broccolini Crustless Quiche",
+        "readyInMinutes": 75,
+        "servings": 6,
+        "sourceUrl": "http://www.musingsofahousewife.com/2010/05/tried-and-true-crustless-quiche.html"
+        },
+        {
+        "id": 189949,
+        "imageType": "jpg",
+        "title": "Lemon-and Prosciutto-Stuffed Pork Loin Roast with Broccolini",
+        "readyInMinutes": 150,
+        "servings": 10,
+        "sourceUrl": "http://www.epicurious.com/recipes/food/views/362531"
+        },
+        {
+        "id": 16654,
+        "imageType": "jpg",
+        "title": "Romaine Salad With Prosciutto Crisps",
+        "readyInMinutes": 14,
+        "servings": 4,
+        "sourceUrl": "http://www.marthastewart.com/331809/romaine-salad-with-prosciutto-crisps"
+        },
+        {
+        "id": 39468,
+        "imageType": "jpg",
+        "title": "Sweet-Potato Soup with Prosciutto Crisps",
+        "readyInMinutes": 90,
+        "servings": 25,
+        "sourceUrl": "http://www.myrecipes.com/recipe/sweet-potato-soup-with-prosciutto-crisps-10000001673084/"
+        },
+        {
+        "id": 57081,
+        "imageType": "jpg",
+        "title": "Fresh Figs With Prosciutto And Parmesan Crisps",
+        "readyInMinutes": 45,
+        "servings": 4,
+        "sourceUrl": "http://www.donnahay.com.au/recipes/entertaining/nibbles/fresh-figs-with-prosciutto-and-parmesan-crisps"
+        },
+        {
+        "id": 301757,
+        "imageType": "jpg",
+        "title": "Seared Scallop Salad with Prosciutto Crisps",
+        "readyInMinutes": 45,
+        "servings": 4,
+        "sourceUrl": "http://www.myrecipes.com/recipe/scallop-salad-prosciutto-crisps-50400000128100/"
+        },
+        {
+        "id": 322037,
+        "imageType": "jpg",
+        "title": "Chicken With Peppers, Broccolini, and Basil",
+        "readyInMinutes": 32,
+        "servings": 4,
+        "sourceUrl": "http://www.myrecipes.com/recipe/chicken-with-peppers-broccolini-basil-10000001918610/"
+        },
+        {
+        "id": 30947,
+        "imageType": "jpg",
+        "title": "Shaved Summer Squash Salad with Prosciutto Crisps",
+        "readyInMinutes": 20,
+        "servings": 4,
+        "sourceUrl": "http://www.myrecipes.com/recipe/shaved-summer-squash-salad-10000001835296/"
+        },
+        {
+            "id": 45905,
+            "imageType": "jpg",
+            "title": "Roasted Pork Loin With Apple, Onion & Dried Plum Stuffing",
+            "readyInMinutes": 167,
+            "servings": 8,
+            "sourceUrl": "http://www.cuisinart.com/recipes/entrees/408.html"
+            },
+            {
+            "id": 293351,
+            "imageType": "jpeg",
+            "title": "Roasted Pork Loin with Roasted Garlic Vinaigrette",
+            "readyInMinutes": 90,
+            "servings": 6,
+            "sourceUrl": "http://www.foodnetwork.com/recipes/giada-de-laurentiis/roasted-pork-loin-with-roasted-garlic-vinaigrette-recipe.html"
+            },
+            {
+            "id": 366657,
+            "imageType": "jpg",
+            "title": "Apple-Cinnamon Pork Loin",
+            "readyInMinutes": 380,
+            "servings": 6,
+            "sourceUrl": "http://www.tasteofhome.com/Recipes/apple-cinnamon-pork-loin"
+            },
+            {
+            "id": 471033,
+            "imageType": "jpg",
+            "title": "Apple Butter Pork Loin",
+            "readyInMinutes": 195,
+            "servings": 6,
+            "sourceUrl": "http://allrecipes.com/Recipe/Apple-Butter-Pork-Loin/Detail.aspx?src=rss"
+            },
+            {
+            "id": 429622,
+            "imageType": "jpg",
+            "title": "Apple-Topped Pork Loin",
+            "readyInMinutes": 95,
+            "servings": 15,
+            "sourceUrl": "http://www.tasteofhome.com/Recipes/apple-topped-pork-loin"
+            },
+            {
+            "id": 81881,
+            "imageType": "jpg",
+            "title": "Apple-glazed Pork Loin",
+            "readyInMinutes": 950,
+            "servings": 3,
+            "sourceUrl": "http://www.williams-sonoma.com/recipe/apple-glazed-pork-loin.html"
+            },
+            {
+            "id": 410407,
+            "imageType": "jpg",
+            "title": "Apple-Glazed Pork Loin",
+            "readyInMinutes": 70,
+            "servings": 12,
+            "sourceUrl": "http://www.tasteofhome.com/recipes/apple-glazed-pork-loin"
+            },
+            {
+            "id": 510706,
+            "imageType": "jpg",
+            "title": "Apple Stuffed Pork Loin",
+            "readyInMinutes": 50,
+            "servings": 3,
+            "sourceUrl": "http://www.peanutbutterandpeppers.com/2012/04/12/apple-stuffed-pork-loin/"
+            },
+            {
+            "id": 311275,
+            "imageType": "jpeg",
+            "title": "Roasted Pork Loin",
+            "readyInMinutes": 80,
+            "servings": 6,
+            "sourceUrl": "http://www.foodnetwork.com/recipes/giada-de-laurentiis/roasted-pork-loin-recipe.html"
+            },
+            {
+            "id": 445985,
+            "imageType": "jpg",
+            "title": "Roasted Pork Loin",
+            "readyInMinutes": 145,
+            "servings": 15,
+            "sourceUrl": "http://www.tasteofhome.com/recipes/roasted-pork-loin"
+            },
+            {
+                "id": 519173,
+                "imageType": "jpg",
+                "title": "Chicken Ranch Burgers",
+                "readyInMinutes": 22,
+                "servings": 4,
+                "sourceUrl": "http://www.julieseatsandtreats.com/2013/03/chicken-ranch-burgers-2/http://"
+                },
+                {
+                "id": 552404,
+                "imageType": "jpg",
+                "title": "Buffalo-Ranch Chicken Burgers",
+                "readyInMinutes": 20,
+                "servings": 5,
+                "sourceUrl": "http://www.bakeyourday.net/buffalo-chicken-burgers/"
+                },
+                {
+                "id": 519362,
+                "imageType": "png",
+                "title": "Buffalo Chicken Ranch Burgers",
+                "readyInMinutes": 22,
+                "servings": 4,
+                "sourceUrl": "http://www.julieseatsandtreats.com/2013/09/buffalo-chicken-ranch-burgers/"
+                },
+                {
+                "id": 575750,
+                "imageType": "jpg",
+                "title": "Buffalo Chicken Burgers with Ranch Slaw",
+                "readyInMinutes": 22,
+                "servings": 4,
+                "sourceUrl": "http://www.bunsinmyoven.com/2013/04/26/buffalo-chicken-burgers-with-ranch-slaw/"
+                },
+                {
+                "id": 471356,
+                "imageType": "jpg",
+                "title": "Ranch Burgers",
+                "readyInMinutes": 25,
+                "servings": 8,
+                "sourceUrl": "http://allrecipes.com/Recipe/Ranch-Burgers/Detail.aspx?src=rss"
+                },
+                {
+                "id": 572049,
+                "imageType": "jpg",
+                "title": "Ranch Burgers",
+                "readyInMinutes": 35,
+                "servings": 3,
+                "sourceUrl": "http://www.ladybehindthecurtain.com/ranch-burgers/"
+                },
+                {
+                "id": 510607,
+                "imageType": "jpg",
+                "title": "Ranch Burgers",
+                "readyInMinutes": 15,
+                "servings": 4,
+                "sourceUrl": "http://www.peanutbutterandpeppers.com/2012/09/14/ranch-burgers/"
+                },
+                {
+                "id": 99387,
+                "imageType": "jpg",
+                "title": "Skillet Ranch Burgers",
+                "readyInMinutes": 30,
+                "servings": 4,
+                "sourceUrl": "http://www.tasteofhome.com/recipes/skillet-ranch-burgers"
+                },
+                {
+                "id": 744632,
+                "imageType": "jpeg",
+                "title": "BLT Ranch Burgers",
+                "readyInMinutes": 45,
+                "servings": 4,
+                "sourceUrl": "http://www.foodnetwork.com/recipes/katie-lee/blt-ranch-burgers.html"
+                },
+                {
+                "id": 305541,
+                "imageType": "jpeg",
+                "title": "Niman Ranch Burgers",
+                "readyInMinutes": 70,
+                "servings": 6,
+                "sourceUrl": "http://www.foodnetwork.com/recipes/ina-garten/niman-ranch-burgers-recipe.html"
+                }
+]);
