8b65ce3af37e0fb6bdc06907579dd3e2f40f13cc
Akwasi Atakora-Acheampong <aa.acheampong1@gmail.com>
2021-08-07 16:10:54 +0100

    index route complete

---
+++ Cooking_starter/server/helpers/create_router.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const ObjectId = require('mongodb').ObjectId;
+
+const createRouter = function(collection) {
+    const router = express.Router();
+
+    router.get('/', (req, res) => {
+        collection
+        .find()
+        .toArray()
+        .then((docs) => res.json(docs))
+        .catch((error) => {
+            console.error(error);
+            res.status(500);
+            res.json({ status: 500, error:error });
+        });
+    });
+
+    router.get('/:id', (req, res) => {
+        
+    })
+}
