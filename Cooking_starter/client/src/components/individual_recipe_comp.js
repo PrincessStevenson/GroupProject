cfafb68412afbc125d7d4a2c93f722d949a31813
Emil <emil.shiva@gmail.com>
2021-08-10 11:30:32 +0100

    ingredients added

--- Cooking_starter/client/src/components/individual_recipe_comp.js
+++ Cooking_starter/client/src/components/individual_recipe_comp.js
@@ -3,6 +3,10 @@ import React from 'react'
 
 const IndividualRecipe = ({individualRecipe}) => {
 
+    const ingredients = individualRecipe.extendedIngredients.map((ingredient) => {
+        return <li> {ingredient.original} </li>
+    });
+
     return (
 
         <section>
@@ -10,8 +14,10 @@ const IndividualRecipe = ({individualRecipe}) => {
            <h1>{individualRecipe.title}</h1>
            <p> Time to Make: {individualRecipe.readyInMinutes} minutes </p>
            <p> Servings: {individualRecipe.servings} people </p>
+           <ul>
+               {ingredients}
+           </ul>
            {/* <p>{individualRecipe.diets}</p> */}
-           <p>{individualRecipe.extendedIngredients.originalString}</p>
            <p>{individualRecipe.instructions}</p>
            {/* <p>{individualRecipe.analyzedInstructions}</p> */}
 
