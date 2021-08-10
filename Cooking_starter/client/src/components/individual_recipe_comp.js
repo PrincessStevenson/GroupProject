a2404bafa79e4d10a5c46d10c913d1667e3ce753
Emil <emil.shiva@gmail.com>
2021-08-10 10:48:23 +0100

    indivial recipe components added

--- Cooking_starter/client/src/components/individual_recipe_comp.js
+++ Cooking_starter/client/src/components/individual_recipe_comp.js
@@ -6,7 +6,15 @@ const IndividualRecipe = ({individualRecipe}) => {
     return (
 
         <section>
+            <img src ={individualRecipe.image} alt= "Image of Recipe" height = "100" width ="125" ></img>
            <h1>{individualRecipe.title}</h1>
+           <p> Time to Make: {individualRecipe.readyInMinutes} minutes </p>
+           <p> Servings: {individualRecipe.servings} people </p>
+           {/* <p>{individualRecipe.diets}</p> */}
+           <p>{individualRecipe.extendedIngredients.originalString}</p>
+           <p>{individualRecipe.instructions}</p>
+           {/* <p>{individualRecipe.analyzedInstructions}</p> */}
+
         </section>
     )
 };
