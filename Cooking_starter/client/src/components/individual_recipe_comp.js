a96ec17b31716230d2bd74c7ac015ba1057a7355
Princess <b.stevenson@hotmail.com>
2021-08-10 15:48:41 +0100

    Individual recipe writing positioned correctly

--- Cooking_starter/client/src/components/individual_recipe_comp.js
+++ Cooking_starter/client/src/components/individual_recipe_comp.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './individual_recipe_component.css'
 
 
 const IndividualRecipe = ({individualRecipe, resetIndvidualRecipe}) => {
@@ -31,17 +32,25 @@ const IndividualRecipe = ({individualRecipe, resetIndvidualRecipe}) => {
     return (
 
         <section>
-            <img src ={individualRecipe.image} alt= "Image of Recipe" height = "100" width ="125" ></img>
-           <h1>{individualRecipe.title}</h1>
-           <p> Time to Make: {individualRecipe.readyInMinutes} minutes </p>
-           <p> Servings: {individualRecipe.servings} people </p>
-           {dietCategory()}
-           
-           <p>Ingredients: </p>
-           <ul>
+            <h1>{individualRecipe.title}</h1>
+            <div class="main-meat">
+
+            <div class= "side-info">
+            <p> Time to Make: {individualRecipe.readyInMinutes} minutes </p>
+            <p> Servings: {individualRecipe.servings} people </p>
+            {dietCategory()}
+            <p>Ingredients: </p>
+            <ul>
                {ingredients}
-           </ul>
-           <p>{individualRecipe.instructions}</p>
+            </ul>
+            </div>
+
+            <img class = "recipe_image" src ={individualRecipe.image} alt= "Image of Recipe" height = "250" width ="350" />
+           </div>
+
+
+           <p class= "instructions">{individualRecipe.instructions}</p>
+           
            
            <button type="button" onClick={handleButtonClick}>Recipe List</button>
            
